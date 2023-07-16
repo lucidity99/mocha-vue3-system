461e22e12db58c934d940bc80cc218f21a301114
lucidity99 <lucidity929@163.com>
2023-07-16 20:52:36 +0800

    fix:限制输入数字指令在切换中文输入法时失效的问题

--- src/directive/inputNumber.ts
+++ src/directive/inputNumber.ts
@@ -14,21 +14,31 @@ export default {
     if (decimal > 0) regDecimal = new RegExp(`^\\d*(.?\\d{0,${decimal}})`, 'g')
     else regDecimal = new RegExp(`^\\d*`, 'g')
 
-    el.inputListener = (e) => {
+    let locking = false
+    elInput.onkeyup = (e) => {
+      if (locking) {
+        return
+      }
       let val = elInput.value
-
       elInput.value =
         val
           .replace(/[^\d^\.]+/g, '')
           .replace(/^0+(\d)/, '$1')
           .replace(/^\./, '0.')
           .match(regDecimal)[0] || ''
-      if (val !== elInput.value) elInput.dispatchEvent(new Event('input'))
+      if (val !== elInput.value && !locking) {
+        elInput.dispatchEvent(new Event('input'))
+      }
     }
-    elInput.addEventListener('input', el.inputListener)
-  },
 
+    elInput.addEventListener('compositionstart', () => {
+      locking = true //解决中文输入双向绑定失效
+    })
+    elInput.addEventListener('compositionend', () => {
+      locking = false //解决中文输入双向绑定失效
+    })
+  },
   unmounted(el: ExHTMLElement) {
-    el.getElementsByTagName('input')[0].removeEventListener('input', el.inputListener)
+    el.getElementsByTagName('keyup')[0].removeEventListener('input', el.inputListener)
   }
 }
--- src/views/comp/infiniteList/index.vue
+++ src/views/comp/infiniteList/index.vue
@@ -1,23 +1,5 @@
 <template>
-  <div id="a">
-    <div class="w-40 h-20" v-for="(item, index) in count">{{ item }}</div>
-  </div>
+  <div id="a">开发中</div>
 </template>
 
-<script setup lang="ts">
-import { ref, onMounted } from 'vue'
-let count = ref(20)
-
-window.addEventListener('scroll', () => {
-  const el = document.getElementById('a')
-  const m = document.querySelector('.main-content')
-  console.log(
-    window.innerHeight,
-    el?.offsetHeight,
-    el?.clientHeight,
-    m?.clientHeight,
-    m?.scrollTop,
-    window.scrollY
-  )
-})
-</script>
+<script setup lang="ts"></script>
