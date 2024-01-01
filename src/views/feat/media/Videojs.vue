a819e8e50afb102ee420c637a60e2346ddb68003
lucidity99 <lucidity929@163.com>
2024-01-01 21:21:22 +0800

    准备加入videojs播放

---
+++ src/views/feat/media/Videojs.vue
@@ -0,0 +1,13 @@
+<!--
+ * @Author: lucidity99 lucidity929@163.com
+ * @Date: 2023-07-21 08:46:00
+ * @LastEditors: lucidity99 lucidity929@163.com
+ * @LastEditTime: 2024-01-01 21:20:18
+ * @FilePath: /mocha-vue3-system/src/views/feat/media/Videojs.vue
+ * @Description:
+ *
+ *
+-->
+<template>
+  <div>使用videojs实现 m3u8 视频播放</div>
+</template>
